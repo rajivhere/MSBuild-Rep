5c4bd4cbdafe057f57f0b49fb40d775a689c878a
Rajiv <rajivhere@gmail.com>
2018-09-23 01:12:46 -0400

    Message removed

--- WpfCustomControl/MainWindow.xaml.cs
+++ WpfCustomControl/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace WpfCustomControl
 
         private void StackPanel_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Bubbled to Stackpanel");
+            //MessageBox.Show("Bubbled to Stackpanel");
             MessageBox.Show("Bubbled to Stackpanel");
         }
     }
